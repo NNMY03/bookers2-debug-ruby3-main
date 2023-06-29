37b758fba07fcf475f3fa26ef737a5cd9f5c466f
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-06-29 06:36:50 +0000

    [Add]いいね用model/contoroller/routes追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorite, dependent: :destroy
+  
+  def favorite_by?(user)
+    favorite.exists?(user_id: user_id)
+  end
+  
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 end
